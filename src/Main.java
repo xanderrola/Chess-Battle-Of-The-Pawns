905d8cbcbe4ae8108ab99640321ac070eb1c4570
clintgalendez <clintgalendez@gmail.com>
2022-10-26 10:20:16 +0800

    added play and pause methods

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,17 @@
 import java.awt.*;
 
 public class Main {
+    /**
+     * Once called, the game of chess will begin, can be easily integrated to the state machine
+     * where the state machine can call the method to play the game.
+     */
+    public static void play() {
+    }
+
+    /**
+     * Once called, the game of chess will pause, can be easily integrated to the state machine
+     * where the state machine can call the method to pause the game.
+     */
+    public static void pause() {
+    }
 }
